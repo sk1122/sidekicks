2054a782497aea8409cd6a0eda3a073d6d5bd27e
= <=>
2022-02-21 13:04:05 +0530

    product page added:

---
+++ components/CryptoKick.tsx
@@ -0,0 +1,14 @@
+export const CryptoKick = () => {
+  return (
+    <>
+      <input
+        className="  border-white-400 placeholder-white-300 mb-[20px] w-full border bg-transparent p-4 text-white"
+        type="text"
+        placeholder="Amount"
+      />
+      <button className="w-full  rounded  bg-[#5551ff] p-4   text-white">
+        Give Crypto Kicks
+      </button>
+    </>
+  )
+}
