c660110b095f020ed86f74c6e63dba2ba054a94c
= <=>
2022-02-23 23:45:01 +0530

    function intrigated on frontend

--- components/productlist/productCard.tsx
+++ components/productlist/productCard.tsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react";
+
+
 export const ProductCard = () => {
-    const MAX_lENGTH = 100;
+
+    const MAX_lENGTH = 1000;
     const text = "Lorem ipsum dolor sit amellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus eius sint asperiores debitis recusandae corrupti animi. Magni laboriosam, harum nisi aliquam facere earum excepturi ratione velit temporibus ipsam sunt amet lorem30  Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio iste provident amet sed, enim necessitatibus nemo? Obcaecati nostrum qui ab? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magnam amet fuga ea aperiam unde accusamus omnis qui quas consequatur Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique adipisci debitis voluptatum dolor possimus quidem r.. "
-    const pera = text.length > MAX_lENGTH ? <p>{text} ...<a href="">Read More</a></p> : <p>{text}</p>
+    const pera = text.length > MAX_lENGTH ? <p>{text.substring(0, MAX_lENGTH)} ...<a href="">Read More</a></p> : <p>{text}</p>
 
 
     return (
