107ab26624ce286e24fdbe70f3aa8a88bf1dc0f7
= <=>
2022-02-24 21:48:13 +0530

    ablity to fetch data on frontend

--- pages/productlist.tsx
+++ pages/productlist.tsx
@@ -1,10 +1,10 @@
-import Navbar from "../components/Navbar"
 import { ProductCard } from "../components/productlist/productCard";
 
 
 import { useState, useEffect } from "react";
 import { useAccountContext } from "./_context";
 const ProductList = () => {
+    const [products, setProducts] = useState([]);
 
     const { getAllProjects } = useAccountContext()
     const [data, setData] = useState([])
@@ -16,7 +16,11 @@ const ProductList = () => {
     }, [])
 
     useEffect(() => {
-        console.log(data)
+        setProducts(data)
+        console.log(products)
+
+        // products.map(product => console.log(product[0]))
+
     }, [data])
 
     return (
@@ -25,24 +29,10 @@ const ProductList = () => {
             <div className="h-full w-screen bg-black">
                 <div className="p-4 m-auto h-full w-[90%] ">
                     <div className=" lg:m-auto justify-center lg:flex flex-wrap max-w-[1000px] 2xl:max-w-full">
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
-                        <ProductCard />
+                        {products.map((product) => {
+                            product = product[0]
+                            return <ProductCard title={product.title} description={product.description} />
+                        })}
                     </div>
 
                 </div>
