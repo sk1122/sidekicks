e0d9d9e94695c42af462f616b7a453de29c470d7
= <=>
2022-03-01 15:34:48 +0530

    validating twitter username has been done

--- components/teams.tsx
+++ components/teams.tsx
@@ -10,19 +10,20 @@ export const Team = ({ creators }) => {
   console.log(creators)
   return (
     <>
-      <div className="w-full px-[15px]">
+      <div className="w-full  px-[15px]">
         <div className=" mt-[30px] flex w-[100%] justify-between  text-white">
           <h2>Team</h2>
           <button onClick={handleHide} className="bg-transparent text-[2rem]">+</button>
         </div>
         {hide ? <div></div> :
-          <div className="mb-20px m-auto  w-[90%] border text-white border-white ">
+          <div className="mb-20px m-auto  w-[90%] min-h-[100px] border text-white border-white ">
             {creators ?
               creators.map(creator => {
-                return <div className="flex items-center text-[1.3rem] underline  mt-[12px] ml-[20px] mb-[20px]  ">
+                return creator.twitter ? <div className="flex items-center text-[1.3rem] underline  mt-[12px] ml-[20px] mb-[20px]  ">
+
                   <img className="w-[30px] mr-3 rounded-full" src="/icon/twitter.gif" alt="" />
                   <a href={"https://twitter.com/" + creator.twitter}>{creator.twitter}</a>
-                </div>
+                </div> : ""
 
               })
               : <div>No team mates</div>
